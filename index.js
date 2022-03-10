950e25ff8848529bf7d956a641cf884f2583cbf7
Ashok Nayak <ashok.nayakii@gmail.com>
2022-03-10 18:59:35 -0500

    more pseudocode

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
 const fs = require('fs');
-
+// Create an empty array in which to place answers
 
 // Update this function to build a manager and ask manager specific questions therefore office number at the end
 
@@ -54,40 +54,41 @@ const firstQuestions = () => {
 
     })
 };
+// use a push method within that then statement to push answers into the array and then console log that array
 
 firstQuestions();
 // Create a menu function to allow user to select which type of employee to create.  This function routes to other functions either Engineer or Intern in which you will
 // ask all 3 questions again and individual question again.
 // Create a function to get the role that we're assigning to the new employee with a checkbox question
 
-const getRole = () => {
-    return inquirer.prompt([
+// const getRole = () => {
+//     return inquirer.prompt([
 
-{
-type: 'checkbox',
-name: 'role',
-message: 'What role will your new employee assume?',
-choices: ['Engineer', 'Manager', 'Intern'],
-validate: roleInput => {
-    if(roleInput) {
-        return true
-    } else {
-        console.log ('You must enter a role for your new team member')
-    }
-}
+// {
+// type: 'checkbox',
+// name: 'role',
+// message: 'What role will your new employee assume?',
+// choices: ['Engineer', 'Manager', 'Intern'],
+// validate: roleInput => {
+//     if(roleInput) {
+//         return true
+//     } else {
+//         console.log ('You must enter a role for your new team member')
+//     }
+// }
 
-},
+// },
 
-])
-}
+// ])
+// }
 
-getRole();
+// getRole();
 // After engineer return to menu function.  
 // How to connect this step to conditional questions for Manager, Intern, and Engineer?
 
-const checkRole = () => {
-    if 
-}
+// const checkRole = () => {
+//     if 
+// }
 
 
 
@@ -99,7 +100,7 @@ const checkRole = () => {
 
 // A writetoFile will be needed in order to return data into a template
 
-const writeToFile = (fileName, data) => {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+// const writeToFile = (fileName, data) => {
+//     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
     
-} 
+// } 
