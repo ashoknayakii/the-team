58b8e1def3c1e9ec0afbd82439e599cf7bafd96b
Ashok Nayak <ashok.nayakii@gmail.com>
2022-04-16 14:29:52 -0400

    enginner and intern questions added

--- index.js
+++ index.js
@@ -1,144 +1,243 @@
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const fs = require('fs');
 // Create an empty array in which to place answers
-const teamMember = []
+const teamMembers = []
 
 // Update this function to build a manager and ask manager specific questions therefore office number at the end
 
-const firstQuestions = () => {
+const buildManager = () => {
     console.log('Build your team!');
-    return inquirer.prompt([
-  
-    {
-        type: "input",
-        name: "name",
-        message: "What is the employee's full name?",
-        validate: answer => {
-            console.log(answer)
-            if (!answer) {
-                return "Please enter at least one value."
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "name",
+            message: "What is the employee's full name?",
+            validate: answer => {
+                console.log(answer)
+                if (!answer) {
+                    return "Please enter at least one value."
+                }
+                return true;
             }
-            return true;
-        }
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the employee's id number",
-        validate: answer => {
-            if (isNaN(answer)) {
-                return "Please enter a numeric value."
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the employee's id number",
+            validate: answer => {
+                if (isNaN(answer)) {
+                    return "Please enter a numeric value."
+                }
+                return true;
             }
-            return true;
-        }
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the employee's email address?",
-        validate: answer => {
-            if (!answer) {
-                return "Please enter a valid email address."
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the employee's email address?",
+            validate: answer => {
+                if (!answer) {
+                    return "Please enter a valid email address."
+                }
+                const passRegex = answer.match(/\S+@\S+\.\S+/);
+                if (!passRegex) {
+                    return "Please enter a valid email address."
+                }
+                return true;
             }
-            const passRegex = answer.match(/\S+@\S+\.\S+/);
-            if (!passRegex) {
-                return "Please enter a valid email address."
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "What is the employee's office number?",
+            validate: answer => {
+                if (!answer) {
+                    return "Please enter at least one value."
+                }
+                return true;
             }
-            return true;
-        }
-    },
-    {
-        type: "input",
-        name: "manager.office",
-        message: "What is the employee's office number?",
-        validate: answer => {
-            if (!answer) {
-                return "Please enter at least one value."
-            }
-            return true;
-        }
-    },
-    {
-        type: 'checkbox',
-        name: 'role',
-        message: 'What role will your new employee assume or have you finished building your team?',
-        choices: ['Engineer', 'Intern', 'Finished Building Team']
-    },
-    // {
-    //     type: "input",
-    //     name: "engineer.github",
-    //     message: "What is the employee's github address?",
-    //     when(answers) {
-    //         return answers.role === "Engineer"
-    //     }
-        
-    // },
-    // {
-    //     type: "input",
-    //     name: "intern.school",
-    //     message: "What school did the intern attend?",
-    //     when(answers) {
-    //         return answers.role === "Intern"
-    //     }
-    // }
+        },
 
 
-]).then(answers => { 
-        // const emp1 = new Employee(answers.name, answers.id, answers.email, answers.office, answers.role);
+    ]).then(answers => {
+        const emp1 = new Employee(answers.name, answers.id, answers.email, answers.office);
         console.log(emp1);
-        // Check to see if answers.role = engineer like line 73 then ask new set of questions 
-        // Switch statement to set up the structure of the role questions
-        // console.log(emp1.firstQuestions());
+        newTeamMember();
+
 
     })
 };
 // use a push method within that then statement to push answers into the array and then console log that array
 
-firstQuestions();
+const newTeamMember = () => {
+    inquirer.prompt([
+        
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What role will your new employee assume or have you finished building your team?',
+            choices: ['Engineer', 'Intern', 'Finished Building Team']
+        },
+
+    ]).then(answer => {
+        console.log(answer)
+        if (answer.role === "Engineer") {
+            console.log("Engineer")
+            engineerQuestion();
+        }
+        else if (answer.role === "Intern") {
+            console.log("Intern")
+            internQuestion();
+        }
+        else {
+            console.log("Finished Building Team!")
+        }
+    })
+}
+
 // Create a menu function to allow user to select which type of employee to create.  This function routes to other functions either Engineer or Intern in which you will
 // ask all 3 questions again and individual question again.
 // Create a function to get the role that we're assigning to the new employee with a checkbox question
 
-const engineerQuestions = () => {
-    return inquirer.prompt([
-  
-    {
-        
-    },
-    
+const engineerQuestion = () => {
+    inquirer.prompt([
 
-]).then(answers => { 
-        const eng1 = new Employee(answers.github);
+        {
+            type: "input",
+            name: "name",
+            message: "What is the employee's full name?",
+            validate: answer => {
+                console.log(answer)
+                if (!answer) {
+                    return "Please enter at least one value."
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the employee's id number",
+            validate: answer => {
+                if (isNaN(answer)) {
+                    return "Please enter a numeric value."
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the employee's email address?",
+            validate: answer => {
+                if (!answer) {
+                    return "Please enter a valid email address."
+                }
+                const passRegex = answer.match(/\S+@\S+\.\S+/);
+                if (!passRegex) {
+                    return "Please enter a valid email address."
+                }
+                return true;
+            }
+        },
+
+        {
+
+            type: "input",
+            name: "github",
+            message: "What is the employee's github address?",
+            when(answers) {
+                return answers.role === "Engineer"
+            }
+
+        }
+
+
+    ]).then(engineerAnswers => {
+        const eng1 = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.github);
         console.log(eng1);
-        console.log(engineerQuestions());
-
+        newTeamMember();
     })
 };
 
 
-// managerQuestions = () =>
 
 
-// getRole();
-// After engineer return to menu function.  
-// How to connect this step to conditional questions for Manager, Intern, and Engineer?
+const internQuestion = () => {
+    inquirer.prompt([
 
-// const checkRole = () => {
-//     if 
-// }
+        {
+            type: "input",
+            name: "name",
+            message: "What is the employee's full name?",
+            validate: answer => {
+                console.log(answer)
+                if (!answer) {
+                    return "Please enter at least one value."
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the employee's id number",
+            validate: answer => {
+                if (isNaN(answer)) {
+                    return "Please enter a numeric value."
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the employee's email address?",
+            validate: answer => {
+                if (!answer) {
+                    return "Please enter a valid email address."
+                }
+                const passRegex = answer.match(/\S+@\S+\.\S+/);
+                if (!passRegex) {
+                    return "Please enter a valid email address."
+                }
+                return true;
+            }
+        },
+
+        {
+            type: "input",
+            name: "school",
+            message: "What school did the intern attend?",
+            when(answers) {
+                return answers.role === "Intern"
+
+            }
+        }
+
+    ]).then(internAnswers => {
+        const i = new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.school);
+        console.log(i);
+        newTeamMember();
+    })
+}
 
 
 
-// Write final questions for Github, Office Number, School and Create Variables in the Then to Place Answers in
+    // Write final questions for Github, Office Number, School and Create Variables in the Then to Place Answers in
 
 
 
 
 
-// A writetoFile will be needed in order to return data into a template
+    // A writetoFile will be needed in order to return data into a template
 
-// const writeToFile = (fileName, data) => {
-//     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-    
-// } 
+    // const writeToFile = (fileName, data) => {
+    //     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+
+    // } 
+    buildManager();
